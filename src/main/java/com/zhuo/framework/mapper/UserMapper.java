11f7a76eb2d59f1ca4e2cfe5734c4895e83827f3
liuchen <liuchen@coohua.com>
2018-02-24 19:13:08 +0800

    finish

--- src/main/java/com/zhuo/framework/mapper/UserMapper.java
+++ src/main/java/com/zhuo/framework/mapper/UserMapper.java
@@ -1,11 +1,13 @@
 package com.zhuo.framework.mapper;
 
 import com.zhuo.framework.model.User;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * Created by liuchen on 18/2/24.
  */
 public interface UserMapper {
 
+    @Select("select * from user where id = #{id}")
     User getUser(int id);
 }
